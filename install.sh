d666ba16e6172182425341556a8d8364cc60240a
Vineet Choudhary <vineetchoudhary@live.in>
2019-04-08 13:07:30 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-APP_NAME='AppBox.app'
-FILE_NAME='AppBox.tar.gz'
-FILE_URL='https://github.com/vineetchoudhary/AppBox-iOSAppsWirelessInstallation/releases/download/2.7.2/AppBox.tar.gz'
+VERSION="2.7.2"
+APP_NAME="AppBox.app"
+FILE_NAME="AppBox.tar.gz"
+FILE_URL="https://github.com/vineetchoudhary/AppBox-iOSAppsWirelessInstallation/releases/download/$VERSION/AppBox.tar.gz"
 APPLICATION_DIR='/Applications'
 
+echo "Downloading AppBox $VERSION..."
 curl -OL $FILE_URL
+echo "Installing AppBox $VERSION..."
 tar -xf $FILE_NAME -C $APPLICATION_DIR
+echo "Starting AppBox $VERSION..."
 open $APPLICATION_DIR/$APP_NAME
