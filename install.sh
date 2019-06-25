8793ccd2b9473c70611ffcacb484db7d4be3d25b
Vineet Choudhary <vineetchoudhary@live.in>
2019-06-25 21:07:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="2.8.0"
+VERSION="2.8.1"
 APP_NAME="AppBox.app"
 FILE_NAME="AppBox.tar.gz"
 FILE_URL="https://github.com/vineetchoudhary/AppBox-iOSAppsWirelessInstallation/releases/download/$VERSION/AppBox.tar.gz"
